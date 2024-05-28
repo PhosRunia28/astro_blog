ffdb8e5d361dd17865661adeb3b52a9082794110
PhosRunia28 <tediphos28@gmail.com>
2024-05-28 21:37:32 +0700

    initial commit

---
+++ src/pages/api/search.json.ts
@@ -0,0 +1,35 @@
+import { APIRoute } from "astro";
+import type { CollectionEntry } from "astro:content";
+import { getCollection } from "astro:content";
+export const GET: APIRoute = async ({ url }) => {
+  const query = url.searchParams.get("query");
+
+  if (query === null) {
+    return new Response(
+      JSON.stringify({
+        error: "Query Params is Missing",
+      }),
+      {
+        status: 400,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  }
+  const allBlogArticle: CollectionEntry<"blog">[] = await getCollection("blog");
+  const searchResult = allBlogArticle.filter((article) => {
+    const titleMatch = article.data.title
+      .toLowerCase()
+      .includes(query!.toLowerCase());
+    const bodyMatch = article.body.toLowerCase().includes(query!.toLowerCase());
+    const slugMatch = article.slug.toLowerCase().includes(query!.toLowerCase());
+    return titleMatch || bodyMatch || slugMatch;
+  });
+  return new Response(JSON.stringify(searchResult), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
